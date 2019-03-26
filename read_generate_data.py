7df8ae1d080322301d0c9375e06ee22d85308133
Shivangi Aneja <shivangi.aneja@tum.de>
2019-03-26 22:00:58 +0100

    test

--- read_generate_data.py
+++ read_generate_data.py
@@ -16,8 +16,10 @@ import matplotlib.pyplot as plt
 from scipy import ndimage
 
 
-train_list = ['4', '5', '7', '070', '14']
-val_list = ['1', '148']
+#train_list = ['4', '5', '7', '070', '14']
+train_list = ['1','4', '5', '070']
+#val_list = ['7', '14' '148']
+val_list = ['148']
 stride = [4, 16, 16]
 patch_size = [8, 32, 32]
 seg_path = 'training'
@@ -44,54 +46,54 @@ def generate_patches(in_array, stride, patch_size):
     return out_array
   #  return np.asarray(out_array)
 
-#for dir_name in train_list:
-#    print(dir_name)
-#    seg_img = sitk.ReadImage(os.path.join(seg_path, dir_name, 'segm.nii.gz'))
-#    seg_array = sitk.GetArrayFromImage(seg_img)
-#    seg_array = seg_array[:, cut:np.shape(seg_array)[1]-cut, cut:np.shape(seg_array)[2]-cut]
-#    
-#    flair_img = sitk.ReadImage(os.path.join(seg_path, dir_name, 'pre','FLAIR.nii.gz'))
-#    flair_array = sitk.GetArrayFromImage(flair_img)
-#    
-#    brain_mask_flair = np.zeros(np.shape(flair_array), dtype = 'float32')
-#    brain_mask_flair[flair_array >=thresh] = 1
-#    brain_mask_flair[flair_array < thresh] = 0
-#    for iii in range(np.shape(flair_array)[0]):
-#        brain_mask_flair[iii,:,:] = scipy.ndimage.morphology.binary_fill_holes(brain_mask_flair[iii,:,:])  #fill the holes inside br
-#    flair_array = flair_array - np.mean(flair_array[brain_mask_flair == 1])
-#    flair_array /= np.std(flair_array[brain_mask_flair == 1])
-#    flair_array = flair_array[:, cut:np.shape(flair_array)[1]-cut, cut:np.shape(flair_array)[2]-cut]
-#    
-#    t1_img = sitk.ReadImage(os.path.join(seg_path, dir_name, 'pre','reg_T1.nii.gz'))
-#    t1_array = sitk.GetArrayFromImage(t1_img)
-#    
-#    brain_mask_t1 = np.zeros(np.shape(t1_array), dtype = 'float32')
-#    brain_mask_t1[t1_array >=thresh] = 1
-#    brain_mask_t1[t1_array < thresh] = 0
-#    for iii in range(np.shape(t1_array)[0]):
-#        brain_mask_t1[iii,:,:] = scipy.ndimage.morphology.binary_fill_holes(brain_mask_t1[iii,:,:])  #fill the holes inside br
-#    t1_array = t1_array - np.mean(t1_array[brain_mask_t1 == 1])
-#    t1_array /= np.std(t1_array[brain_mask_t1 == 1])
-#    t1_array = t1_array[:, cut:np.shape(t1_array)[1]-cut, cut:np.shape(t1_array)[2]-cut]
-#    
-#    patch_seg = generate_patches(seg_array, stride, patch_size)
-#    patch_flair = generate_patches(flair_array, stride, patch_size)
-#    patch_t1 = generate_patches(t1_array, stride, patch_size)
-#    
-#    patch_seg_ = np.concatenate([arr[np.newaxis] for arr in patch_seg])
-#    patch_seg_ = patch_seg_[..., np.newaxis]
-#    patch_flair_ = np.concatenate([arr[np.newaxis] for arr in patch_flair])
-#    patch_t1_ = np.concatenate([arr[np.newaxis] for arr in patch_t1])
-#    patch_flair_t1 = np.concatenate((patch_flair_[..., np.newaxis], patch_t1_[..., np.newaxis]), axis = 4)
-#    
-#    train_data.extend(patch_flair_t1)
-#    train_gt.extend(patch_seg_)
-#        
-#train_data_ = np.concatenate([arr[np.newaxis] for arr in train_data])   
-#train_gt_ = np.concatenate([arr[np.newaxis] for arr in train_gt])     
-#np.save('train_data.npy', train_data_)
-#np.save('train_gt.npy', train_gt_)    
-#        
+for dir_name in train_list:
+   print(dir_name)
+   seg_img = sitk.ReadImage(os.path.join(seg_path, dir_name, 'segm.nii.gz'))
+   seg_array = sitk.GetArrayFromImage(seg_img)
+   seg_array = seg_array[:, cut:np.shape(seg_array)[1]-cut, cut:np.shape(seg_array)[2]-cut]
+
+   flair_img = sitk.ReadImage(os.path.join(seg_path, dir_name, 'pre','FLAIR.nii.gz'))
+   flair_array = sitk.GetArrayFromImage(flair_img)
+
+   brain_mask_flair = np.zeros(np.shape(flair_array), dtype = 'float32')
+   brain_mask_flair[flair_array >=thresh] = 1
+   brain_mask_flair[flair_array < thresh] = 0
+   for iii in range(np.shape(flair_array)[0]):
+       brain_mask_flair[iii,:,:] = scipy.ndimage.morphology.binary_fill_holes(brain_mask_flair[iii,:,:])  #fill the holes inside br
+   flair_array = flair_array - np.mean(flair_array[brain_mask_flair == 1])
+   flair_array /= np.std(flair_array[brain_mask_flair == 1])
+   flair_array = flair_array[:, cut:np.shape(flair_array)[1]-cut, cut:np.shape(flair_array)[2]-cut]
+
+   t1_img = sitk.ReadImage(os.path.join(seg_path, dir_name, 'pre','reg_T1.nii.gz'))
+   t1_array = sitk.GetArrayFromImage(t1_img)
+
+   brain_mask_t1 = np.zeros(np.shape(t1_array), dtype = 'float32')
+   brain_mask_t1[t1_array >=thresh] = 1
+   brain_mask_t1[t1_array < thresh] = 0
+   for iii in range(np.shape(t1_array)[0]):
+       brain_mask_t1[iii,:,:] = scipy.ndimage.morphology.binary_fill_holes(brain_mask_t1[iii,:,:])  #fill the holes inside br
+   t1_array = t1_array - np.mean(t1_array[brain_mask_t1 == 1])
+   t1_array /= np.std(t1_array[brain_mask_t1 == 1])
+   t1_array = t1_array[:, cut:np.shape(t1_array)[1]-cut, cut:np.shape(t1_array)[2]-cut]
+
+   patch_seg = generate_patches(seg_array, stride, patch_size)
+   patch_flair = generate_patches(flair_array, stride, patch_size)
+   patch_t1 = generate_patches(t1_array, stride, patch_size)
+
+   patch_seg_ = np.concatenate([arr[np.newaxis] for arr in patch_seg])
+   patch_seg_ = patch_seg_[..., np.newaxis]
+   patch_flair_ = np.concatenate([arr[np.newaxis] for arr in patch_flair])
+   patch_t1_ = np.concatenate([arr[np.newaxis] for arr in patch_t1])
+   patch_flair_t1 = np.concatenate((patch_flair_[..., np.newaxis], patch_t1_[..., np.newaxis]), axis = 4)
+
+   train_data.extend(patch_flair_t1)
+   train_gt.extend(patch_seg_)
+
+train_data_ = np.concatenate([arr[np.newaxis] for arr in train_data])
+train_gt_ = np.concatenate([arr[np.newaxis] for arr in train_gt])
+np.save('train_data.npy', train_data_)
+np.save('train_gt.npy', train_gt_)
+
 
 for dir_name in val_list:   
     print(dir_name)
